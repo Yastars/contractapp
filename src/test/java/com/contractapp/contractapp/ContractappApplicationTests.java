823001e6b3c5cce7bb0d66f9ad15431ad67ce181
Yastars <yasstars@hotmail.fr>
2020-09-19 17:04:45 +0200

    Spring Initializer

---
+++ src/test/java/com/contractapp/contractapp/ContractappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractapp.contractapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
